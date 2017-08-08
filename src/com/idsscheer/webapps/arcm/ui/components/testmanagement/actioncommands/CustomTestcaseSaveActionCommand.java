bd531da886b63ad0bc08ab2257466d0777175f7f
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-08-08 18:38:01 -0300

    Correções EV105079

--- src/com/idsscheer/webapps/arcm/ui/components/testmanagement/actioncommands/CustomTestcaseSaveActionCommand.java
+++ src/com/idsscheer/webapps/arcm/ui/components/testmanagement/actioncommands/CustomTestcaseSaveActionCommand.java
@@ -597,7 +597,9 @@ public class CustomTestcaseSaveActionCommand extends TestcaseSaveActionCommand {
 				IAppObj controlUpdObj = controlFacade.load(controlOVID, true);
 				controlFacade.allocateLock(controlOVID, LockType.FORCEWRITE);
 				
-				if(this.fernanda.equals("ineffective")){
+				//REO 08.08.2017 - EV105079
+				//if(this.fernanda.equals("ineffective")){ 
+				if(this.fernanda.equals("noneffective")){
 					controlUpdObj.getAttribute(IControlAttributeTypeCustom.ATTR_CUSTOM_STATUS).setRawValue("ineficaz");
 				}else{
 					controlUpdObj.getAttribute(IControlAttributeTypeCustom.ATTR_CUSTOM_STATUS).setRawValue("eficaz");
