777d6e9ecf84eaaf14f9bf7ef673c8764b7d2478
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-09-15 17:04:28 -0300

    Criação de popup - Avaliacao Risco Corporativo

--- src/com/idsscheer/webapps/arcm/ui/components/hierarchy/actioncommands/CustomCREvaluation.java
+++ src/com/idsscheer/webapps/arcm/ui/components/hierarchy/actioncommands/CustomCREvaluation.java
@@ -1,13 +1,21 @@
 package com.idsscheer.webapps.arcm.ui.components.hierarchy.actioncommands;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.servlet.jsp.tagext.BodyContent;
+
 import com.idsscheer.webapps.arcm.bl.models.dialog.DialogRequest;
 import com.idsscheer.webapps.arcm.bl.models.dialog.IBLDialogModel;
 import com.idsscheer.webapps.arcm.bl.models.dialog.IDialogRequest;
 import com.idsscheer.webapps.arcm.bl.navigation.stack.IPage;
+import com.idsscheer.webapps.arcm.bl.navigation.toolbar.BreadcrumbStackEvent;
 import com.idsscheer.webapps.arcm.config.metadata.actioncommand.ActionCommandId;
 import com.idsscheer.webapps.arcm.ui.framework.actioncommands.ActionCommandIds;
 import com.idsscheer.webapps.arcm.ui.framework.actioncommands.object.BaseCacheActionCommand;
 import com.idsscheer.webapps.arcm.ui.framework.dialog.InputDialog;
+import com.idsscheer.webapps.arcm.ui.web.taglib.PopUpTag;
 
 public class CustomCREvaluation extends BaseCacheActionCommand{
 	
@@ -23,16 +31,18 @@ public class CustomCREvaluation extends BaseCacheActionCommand{
 		this.environment.getDialogManager().getNotificationDialog().addWarning(js);*/
 		
 		//Confirmation Dialog
-		IPage page = this.environment.getBreadcrumbStack().peek().getPage();
-		String[] arr = new String[1];
-		IBLDialogModel dialogModel = this.environment.getDialogManager().createConfirmationDialog(this.requestContext, "okcancel", "TESTE2", arr);
+		//IPage page = this.environment.getBreadcrumbStack().peek().getPage();
+		//String[] arr = new String[1];
+		//IBLDialogModel dialogModel = this.environment.getDialogManager().createConfirmationDialog(this.requestContext, "okcancel", "TESTE2", arr);
 		//this.environment.getBreadcrumbStack().peek().addDialog(dialogModel).getDialog().setStatusFromNewToRunning();
 		
 		//Testes para criação de Janela Pop Up
-		DialogRequest dialogReq = new DialogRequest("no_object_lock");
+		//DialogRequest dialogReq = new DialogRequest("custom_cr_evaluation");
 		//IDialogRequest dialogReq = this.environment.getBreadcrumbStack().peek().getFlow().getDialogRequest();
-		IBLDialogModel dialogModel1 = this.environment.getDialogManager().createDynamicDialog(this.requestContext, dialogReq);
-		this.environment.getBreadcrumbStack().peek().addDialog(dialogModel1);//.getDialog().setStatusFromNewToRunning();
+		//IBLDialogModel dialogModel1 = this.environment.getDialogManager().createDynamicDialog(this.requestContext, dialogReq);
+		//this.environment.getBreadcrumbStack().peek().addDialog(dialogModel1);//.getDialog().setStatusFromNewToRunning();
+		
+		this.notificationDialog.addMessage("TESTE");
 		
 	}
 
