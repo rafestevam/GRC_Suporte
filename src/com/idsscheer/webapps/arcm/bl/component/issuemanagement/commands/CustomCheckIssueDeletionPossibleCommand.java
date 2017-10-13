79b97b26429d132ce1943f38947e1a8bb4d4cf80
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-10-13 17:07:54 -0300

    EV115930 - Ajuste da classe de Check Delete Issue

--- src/com/idsscheer/webapps/arcm/bl/component/issuemanagement/commands/CustomCheckIssueDeletionPossibleCommand.java
+++ src/com/idsscheer/webapps/arcm/bl/component/issuemanagement/commands/CustomCheckIssueDeletionPossibleCommand.java
@@ -1,16 +1,26 @@
 package com.idsscheer.webapps.arcm.bl.component.issuemanagement.commands;
 
-import java.util.Collections;
-import java.util.List;
 
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import com.idsscheer.webapps.arcm.bl.authentication.context.ContextFactory;
 import com.idsscheer.webapps.arcm.bl.authentication.context.IUserContext;
+import com.idsscheer.webapps.arcm.bl.authentication.context.IUserRights;
+import com.idsscheer.webapps.arcm.bl.common.support.AppObjUtility;
 import com.idsscheer.webapps.arcm.bl.exception.BLException;
 import com.idsscheer.webapps.arcm.bl.framework.command.CommandContext;
 import com.idsscheer.webapps.arcm.bl.framework.command.CommandResult;
 import com.idsscheer.webapps.arcm.bl.framework.command.IChainContext;
 import com.idsscheer.webapps.arcm.bl.framework.command.ICommand;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObj;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.IClientAppObj;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IEnumAttribute;
+import com.idsscheer.webapps.arcm.common.constants.metadata.Enumerations;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IIssueAttributeType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IIssueAttributeTypeCustom;
 import com.idsscheer.webapps.arcm.common.notification.INotificationList;
@@ -19,8 +29,10 @@ import com.idsscheer.webapps.arcm.common.notification.NotificationTypeEnum;
 import com.idsscheer.webapps.arcm.common.util.ARCMCollections;
 import com.idsscheer.webapps.arcm.common.util.ovid.OVIDUtility;
 import com.idsscheer.webapps.arcm.config.metadata.enumerations.IEnumerationItem;
+import com.idsscheer.webapps.arcm.config.metadata.rights.roles.RoleLevel;
 
 public class CustomCheckIssueDeletionPossibleCommand extends CheckIssueDeletionPossibleCommand implements ICommand {
+	private static final IUserContext fullGrant = ContextFactory.getFullReadAccessUserContext(Locale.ENGLISH);
 	
 	@Override
 	public CommandResult execute(CommandContext cc) throws BLException {
@@ -44,7 +56,7 @@ public class CustomCheckIssueDeletionPossibleCommand extends CheckIssueDeletionP
 		IUserContext ctx = chainCtx.getUserContext();
 		boolean deletionAllowed = false;
 
-		if ((!(deletionAllowed)) && (isManagerForThisIssue(issue, ctx))
+		if ((!(deletionAllowed)) && (this.isManagerForThisIssue(issue, ctx))
 				&& (((super.isInWorkflowState(issue, "apOpenForExecution"))
 						|| (super.isInWorkflowState(issue, "apOpenForCreation"))))) {
 			deletionAllowed = true;
@@ -85,5 +97,53 @@ public class CustomCheckIssueDeletionPossibleCommand extends CheckIssueDeletionP
 
 		return result;		
 	}
-
+	
+	@Override
+	protected boolean isManagerForThisIssue(IAppObj issue, IUserContext ctx) {
+		// TODO Auto-generated method stub
+		//return super.isManagerForThisIssue(issue, ctx);
+		IUserRights userRights = ctx.getUserRights();
+	    boolean result = false;
+	    
+	    if (userRights.hasRole(Enumerations.USERROLE_TYPE.ISSUEMANAGER, Enumerations.USERROLE_LEVEL.CROSSCLIENT)){
+		      return true;
+	    }else{
+	    	if (userRights.hasRole(Enumerations.USERROLE_TYPE.ISSUEMANAGER, Enumerations.USERROLE_LEVEL.CLIENT)){
+				return true;
+	    	}
+	    }
+	    
+	    return result;
+	}
+	
+	/*protected boolean isManagerForThisIssue(IAppObj issue, IUserContext ctx){
+	    IUserRights userRights = ctx.getUserRights();
+	    boolean result = false;
+	    
+	    if (userRights.hasRole(Enumerations.USERROLE_TYPE.ISSUEMANAGER, Enumerations.USERROLE_LEVEL.CROSSCLIENT)){
+	      return true;
+	    }else{
+			List<IClientAppObj>  clients;
+			Iterator i;
+			IAppObj iro;
+			if (userRights.hasRole(Enumerations.USERROLE_TYPE.ISSUEMANAGER, Enumerations.USERROLE_LEVEL.CLIENT)){
+				result = false;
+				List<IAppObj> iros = issue.getAttribute(IIssueAttributeType.LIST_ISSUERELEVANTOBJECTS).getElements(fullGrant);
+				if (!(iros.isEmpty())){
+					clients = ctx.getUserRelations().getClients(Enumerations.USERROLE_TYPE.ISSUEMANAGER, new RoleLevel[] { RoleLevel.C });
+					for (i = iros.iterator(); i.hasNext(); ){
+						iro = (IAppObj)i.next();
+						for (IClientAppObj clientAppObj : clients){
+							if (AppObjUtility.hasRelationToClient(iro, clientAppObj)) {
+								return true;
+							}
+						}
+					}
+				}else {
+					return false; 
+				}
+			}
+	    }
+	    return result;
+	}*/
 }
