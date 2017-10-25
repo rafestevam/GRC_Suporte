356f601bd817ffcd793b86b38c52af22559f79d3
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-10-25 17:24:24 -0200

    EV115928 - Ajuste da classe de delete IRO

--- src/com/idsscheer/webapps/arcm/ui/components/issuemanagement/actioncommands/CustomIRODeleteActionCommand.java
+++ src/com/idsscheer/webapps/arcm/ui/components/issuemanagement/actioncommands/CustomIRODeleteActionCommand.java
@@ -1,9 +1,15 @@
 package com.idsscheer.webapps.arcm.ui.components.issuemanagement.actioncommands;
 
-import java.util.List;
-
 import com.idsscheer.webapps.arcm.bl.exception.RightException;
+import com.idsscheer.webapps.arcm.bl.framework.workflow.IWorkflowTransition;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObj;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.IIssuerelevantobjectAppObj;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjIterator;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjQuery;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.QueryRestriction;
+import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IIssuerelevantobjectAttributeType;
 import com.idsscheer.webapps.arcm.common.util.ovid.IOVID;
 
 public class CustomIRODeleteActionCommand extends IssueRelevantObjectDeleteActionCommand {
@@ -13,23 +19,30 @@ public class CustomIRODeleteActionCommand extends IssueRelevantObjectDeleteActio
 		// TODO Auto-generated method stub
 		super.loadApplicationObjects();
 		
-		List<IOVID> iros = this.environment.getUserContext().getUserRelations().getMarkedAsIssueRelevantObjectIds();
-		for (IOVID id : iros) {
-			for(IOVID iovid : this.requestContext.getSelectedOVIDList()){
-				if(iovid.getId() == id.getId()){
+		for(IOVID id : this.requestContext.getSelectedOVIDList()){
+			try{
+				IAppObj loadedAppObj = this.appObjFacade.load(id, true);
+				if (null != loadedAppObj){
+					long objid = loadedAppObj.getAttribute(IIssuerelevantobjectAttributeType.ATTR_OBJECT_ID).getRawValue();
 					
-					try {
-						IAppObj loadedAppObj = this.appObjFacade.load(id, true);
-						if (null != loadedAppObj) {
-							this.model.addObject(loadedAppObj);
-							this.environment.getAppObjFacade(loadedAppObj.getObjectType())
-									.allocateWriteLock(loadedAppObj.getVersionData().getOVID());
+					IAppObjQuery objQuery = FacadeFactory.getInstance().getAppObjFacade(getFullGrantUserContext(), ObjectType.ISSUERELEVANTOBJECT).createQuery();
+					objQuery.addRestriction(QueryRestriction.eq(IIssuerelevantobjectAttributeType.ATTR_OBJECT_ID, objid));
+					objQuery.addRestriction(QueryRestriction.eq(IIssuerelevantobjectAppObj.BASE_ATTR_CREATOR_USER_ID, Long.valueOf(this.environment.getUserContext().getUserID().getId())));
+					objQuery.addRestriction(QueryRestriction.isNull(IIssuerelevantobjectAppObj.ATTR_IRO_ISSUEID));
+					
+					IAppObjIterator it = objQuery.getResultIterator();
+					while(it.hasNext()){
+						IAppObj deleteAppObj = it.next();
+						if(!this.model.getCurrentObject().equals(deleteAppObj)){
+							this.model.addObject(deleteAppObj);
+							this.environment.getAppObjFacade(deleteAppObj.getObjectType()).allocateWriteLock(deleteAppObj.getVersionData().getOVID());	
 						}
-					} catch (RightException e) {
-						this.log.error("loadApplicationObjects failed.", e);
 					}
 					
 				}
+				
+			}catch (RightException e) {
+				this.log.error("loadApplicationObjects failed.", e);
 			}
 		}
 		
