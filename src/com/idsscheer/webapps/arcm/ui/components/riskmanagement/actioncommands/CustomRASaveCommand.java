2e185d3ed4b14c13a05310e3f993f004a79c4ba6
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-11-06 17:35:03 -0200

    EV118286 - Ajustes na classe CustomRASave

--- src/com/idsscheer/webapps/arcm/ui/components/riskmanagement/actioncommands/CustomRASaveCommand.java
+++ src/com/idsscheer/webapps/arcm/ui/components/riskmanagement/actioncommands/CustomRASaveCommand.java
@@ -10,6 +10,8 @@ import com.idsscheer.webapps.arcm.bl.authentication.context.IUserContext;
 import com.idsscheer.webapps.arcm.bl.exception.RightException;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObj;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObjFacade;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IEnumAttribute;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IStringAttribute;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
 import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskAttributeType;
@@ -17,13 +19,60 @@ import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskAttri
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskassessmentAttributeType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskassessmentAttributeTypeCustom;
 import com.idsscheer.webapps.arcm.common.notification.NotificationTypeEnum;
+import com.idsscheer.webapps.arcm.common.util.ARCMCollections;
 import com.idsscheer.webapps.arcm.common.util.ovid.IOVID;
+import com.idsscheer.webapps.arcm.config.metadata.enumerations.IEnumerationItem;
+import com.idsscheer.webapps.arcm.custom.corprisk.CustomProcRiskResidualCalc;
 import com.idsscheer.webapps.arcm.services.framework.batchserver.services.lockservice.LockType;
 import com.idsscheer.webapps.arcm.ui.framework.actioncommands.object.BaseSaveActionCommand;
 import com.idsscheer.webapps.arcm.ui.framework.common.IUIEnvironment;
 import com.idsscheer.webapps.arcm.ui.framework.common.JobUIEnvironment;
 
 public class CustomRASaveCommand extends BaseSaveActionCommand {
+	
+	
+	//Inicio REO 06.11.2017 - EV118286
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		super.execute();
+		
+		IAppObj raObj = this.formModel.getAppObj();
+		IEnumAttribute reviewerStatusAttr = raObj.getAttribute(IRiskassessmentAttributeType.ATTR_REVIEWER_STATUS);
+		IEnumerationItem reviewerStatus = ARCMCollections.extractSingleEntry(reviewerStatusAttr.getRawValue(), true);
+		
+		try{
+		
+		if(reviewerStatus.equals("completed")){
+			JobUIEnvironment jobEnv = new JobUIEnvironment(getFullGrantUserContext());
+			IUserContext jobCtx = jobEnv.getUserContext();
+			IAppObjFacade rskAppFacade = FacadeFactory.getInstance().getAppObjFacade(jobCtx, ObjectType.RISK);
+			
+			String riscoPotencial = raObj.getAttribute(IRiskassessmentAttributeTypeCustom.ATTR_RESULT_ASSESSMENT).getRawValue();
+			List<IAppObj> currRskList = raObj.getAttribute(IRiskassessmentAttributeType.LIST_RISK).getElements(jobEnv.getUserContext());
+			for (IAppObj riskObj : currRskList) {
+				rskAppFacade.allocateLock(riskObj.getVersionData().getHeadOVID(), LockType.FORCEWRITE);
+				String riskName = riskObj.getAttribute(IRiskAttributeType.ATTR_NAME).getRawValue();
+				IStringAttribute classFinalAttr = riskObj.getAttribute(IRiskAttributeTypeCustom.ATTR_RA_CONTROLFINAL);
+				IStringAttribute residualFinalAttr = riskObj.getAttribute(IRiskAttributeTypeCustom.ATTR_RA_RESIDUALFINAL);
+				String classFinal = classFinalAttr.isEmpty() ? "" : classFinalAttr.getRawValue();
+				String residualFinal = residualFinalAttr.isEmpty() ? "" : residualFinalAttr.getRawValue();
+				if((!classFinal.equals("")) && (residualFinal.equals(""))){
+					CustomProcRiskResidualCalc residualCalc = new CustomProcRiskResidualCalc(riskName, riscoPotencial, classFinal);
+					residualCalc.calculateResidualFinal();
+					riskObj.getAttribute(IRiskAttributeTypeCustom.ATTR_RA_RESIDUALFINAL).setRawValue(residualCalc.getResidualFinal());
+					rskAppFacade.save(riskObj, getDefaultTransaction(), true);
+				}
+				rskAppFacade.releaseLock(riskObj.getVersionData().getHeadOVID());
+			}
+		}
+		
+		}catch(Exception e){
+			this.formModel.addControlInfoMessage(NotificationTypeEnum.ERROR, e.getMessage(), new String[] { getStringRepresentation(this.formModel.getAppObj()) });
+		}
+	
+	}
+	//Fim REO 06.11.2017 - EV118286
 		
 	protected void afterExecute(){
 		
