b56073a87bdb2511e58c237c4cc227eeded3a0db
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-11-06 15:25:27 -0200

    EV114258 - Preparacao da classe CustomDueCheckClass

--- src/com/idsscheer/webapps/arcm/bl/component/riskmanagement/commands/CustomDueCheckCommand.java
+++ src/com/idsscheer/webapps/arcm/bl/component/riskmanagement/commands/CustomDueCheckCommand.java
@@ -10,7 +10,9 @@ public class CustomDueCheckCommand extends DueCheckCommand {
 	protected boolean isDue(IAppObj appObj, CommandContext cc) {
 		// TODO Auto-generated method stub
 		if(super.isDue(appObj, cc)){
-			
+			//Resolver aqui a logica para definição se deve ser gerada nova avaliação de risco
+			//-> TRUE - Continua logica normal para geração de Avaliação de Risco
+			//-> FALSE - Não gera Avaliação de risco
 		}
 		return super.isDue(appObj, cc);
 	}
