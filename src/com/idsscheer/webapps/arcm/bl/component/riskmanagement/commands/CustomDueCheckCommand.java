c623c7f5d051bddf05a2f3189cba8353c65d8eb7
Rafael Estevam <rafael.estevam@exedconsulting.com>
2017-11-09 15:11:26 -0200

    EV114258 - Ajustes classe CustomDueCheckCommand

--- src/com/idsscheer/webapps/arcm/bl/component/riskmanagement/commands/CustomDueCheckCommand.java
+++ src/com/idsscheer/webapps/arcm/bl/component/riskmanagement/commands/CustomDueCheckCommand.java
@@ -1,8 +1,27 @@
 package com.idsscheer.webapps.arcm.bl.component.riskmanagement.commands;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.idsscheer.webapps.arcm.bl.authentication.context.IUserContext;
 import com.idsscheer.webapps.arcm.bl.component.common.command.job.DueCheckCommand;
+import com.idsscheer.webapps.arcm.bl.dataaccess.query.IViewQuery;
+import com.idsscheer.webapps.arcm.bl.dataaccess.query.QueryFactory;
 import com.idsscheer.webapps.arcm.bl.framework.command.CommandContext;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObj;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObjFacade;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.IViewObj;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IEnumAttribute;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
+import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskassessmentAttributeType;
+import com.idsscheer.webapps.arcm.common.util.ARCMCollections;
+import com.idsscheer.webapps.arcm.common.util.ovid.IOVID;
+import com.idsscheer.webapps.arcm.common.util.ovid.OVIDFactory;
+import com.idsscheer.webapps.arcm.config.metadata.enumerations.IEnumerationItem;
+import com.idsscheer.webapps.arcm.ui.framework.common.JobUIEnvironment;
 
 public class CustomDueCheckCommand extends DueCheckCommand {
 	
@@ -10,11 +29,82 @@ public class CustomDueCheckCommand extends DueCheckCommand {
 	protected boolean isDue(IAppObj appObj, CommandContext cc) {
 		// TODO Auto-generated method stub
 		if(super.isDue(appObj, cc)){
-			//Resolver aqui a logica para definição se deve ser gerada nova avaliação de risco
-			//-> TRUE - Continua logica normal para geração de Avaliação de Risco
-			//-> FALSE - Não gera Avaliação de risco
+			
+			boolean hasAssessment = false;
+			List<IAppObj> riskList = appObj.getAttribute(IRiskassessmentAttributeType.LIST_RISK).getElements(cc.getChainContext().getUserContext());
+			for (IAppObj riskObj : riskList) {
+				hasAssessment = this.getAssessment4Risk(riskObj, cc);
+			}
+			return hasAssessment;
+			
 		}
 		return super.isDue(appObj, cc);
+	}
+	
+	private boolean getAssessment4Risk(IAppObj riskObj, CommandContext cc){
+		
+		boolean bReturn = false;
+		
+		IUserContext jobCtx = getJobContext(cc);
+		
+		try {
+			long raID = getRiskAssessment(riskObj, cc, jobCtx);
+			
+			IAppObjFacade facade = FacadeFactory.getInstance().getAppObjFacade(jobCtx, ObjectType.RISKASSESSMENT);
+			IOVID raOVID = OVIDFactory.getOVID(raID);
+			IAppObj raObj = facade.load(raOVID, true);
+			
+			IEnumAttribute statusAttr = raObj.getAttribute(IRiskassessmentAttributeType.ATTR_OWNER_STATUS);
+			IEnumerationItem status = ARCMCollections.extractSingleEntry(statusAttr.getRawValue(), true);
+			
+			if(!status.getId().equals("new")){
+				bReturn = true;
+			}else{
+				bReturn = false;
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return bReturn;
+		
+	}
+
+	private long getRiskAssessment(IAppObj riskObj, CommandContext cc, IUserContext jobCtx) throws Exception {
+		Map filterMap = new HashMap();
+		filterMap.put("r_id", riskObj.getObjectId());
+		long raID = 0;
+		
+		IViewQuery query = QueryFactory.createQuery(jobCtx, "ra2risk", filterMap, null,
+				true, cc.getChainContext().getTransaction());
+		
+		try{
+		
+			Iterator itQuery = query.getResultIterator();
+			
+			while(itQuery.hasNext()){
+				
+				IViewObj viewObj = (IViewObj)itQuery.next();
+				raID = (Long)viewObj.getRawValue("ra_id");
+				
+			}
+		
+		}catch(Exception e){
+			query.release();
+			throw e;
+		}finally{
+			query.release();
+		}
+		
+		return raID;
+	}
+
+	private IUserContext getJobContext(CommandContext cc) {
+		JobUIEnvironment jobEnv = new JobUIEnvironment(cc.getChainContext().getUserContext());
+		IUserContext jobCtx = jobEnv.getUserContext();
+		return jobCtx;
 	}
 
 }
