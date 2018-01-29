406f0ad1e476a9d364471db48a39795e30a3f02b
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-01-29 17:49:58 -0200

    EV131567 - Ajustes nas views de PA e criacao do ViewHandler

--- src/com/idsscheer/webapps/arcm/dl/framework/viewhandler/CustomIssues3ViewHandler.java
+++ src/com/idsscheer/webapps/arcm/dl/framework/viewhandler/CustomIssues3ViewHandler.java
@@ -1,14 +1,15 @@
 package com.idsscheer.webapps.arcm.dl.framework.viewhandler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.apache.myfaces.shared.util.LocaleUtils;
 
@@ -20,101 +21,123 @@ import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjIterator;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjQuery;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IQueryExpression;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IQueryRestriction;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.QueryRestriction;
 import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IIssueAttributeType;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IIssueAttributeTypeCustom;
+import com.idsscheer.webapps.arcm.config.metadata.objecttypes.ITextAttributeType;
 import com.idsscheer.webapps.arcm.dl.framework.BusViewException;
+import com.idsscheer.webapps.arcm.dl.framework.DataLayerComparator;
 import com.idsscheer.webapps.arcm.dl.framework.IDataLayerObject;
 import com.idsscheer.webapps.arcm.dl.framework.IFilterCriteria;
+import com.idsscheer.webapps.arcm.dl.framework.IQueryDefinition;
+import com.idsscheer.webapps.arcm.dl.framework.IQueryObjectDefinition;
 import com.idsscheer.webapps.arcm.dl.framework.IRightsFilterCriteria;
 import com.idsscheer.webapps.arcm.dl.framework.dllogic.QueryDefinition;
+import com.idsscheer.webapps.arcm.dl.framework.dllogic.SimpleFilterCriteria;
+import com.idsscheer.webapps.arcm.ndl.IFilterFactory;
+import com.idsscheer.webapps.arcm.ndl.PersistenceAPI;
 
 public class CustomIssues3ViewHandler implements IViewHandler {
 
 	@Override
-	public void handleView(QueryDefinition query, List<IRightsFilterCriteria> rightsFilter, List<IFilterCriteria> filters,
-			IDataLayerObject currentObject, QueryDefinition parentQuery) throws BusViewException {
+	public void handleView(QueryDefinition query, List<IRightsFilterCriteria> rightsFilter,
+			List<IFilterCriteria> filters, IDataLayerObject currentObject, QueryDefinition parentQuery)
+					throws BusViewException {
 		// TODO Auto-generated method stub
-		
-		Map<Integer, Integer> resultMap = new HashMap<>();
-		if(currentObject == null){
-			IUserContext userCtx = ContextFactory.getFullReadAccessUserContext(LocaleUtils.toLocale("US"));
-			IAppObjFacade facade = FacadeFactory.getInstance().getAppObjFacade(userCtx, ObjectType.ISSUE);
-			
-			IAppObjQuery appQuery = facade.createQuery();
-			
-			IQueryExpression restr1 = QueryRestriction.eq(IIssueAttributeType.BASE_ATTR_VERSION_ACTIVE, true);
-			appQuery.addRestriction(restr1);
-			
-			IAppObjIterator iterator = appQuery.getResultIterator();
-			while(iterator.hasNext()){
-				IAppObj issueObj = iterator.next();
-				Integer obj_id = (int)issueObj.getObjectId();
-				Integer version_number = Integer.valueOf(
-						issueObj.getAttribute(IIssueAttributeType.BASE_ATTR_VERSION_NUMBER).getRawValue().intValue()
-						);
-				resultMap.put(obj_id, version_number);
+
+		// Inicio REO - 29.01.2017 - EV131567
+		Map<Integer, Integer> resultMap = new TreeMap<>();
+		// if (currentObject == null) {
+		IUserContext userCtx = ContextFactory.getFullReadAccessUserContext(LocaleUtils.toLocale("US"));
+		IAppObjFacade facade = FacadeFactory.getInstance().getAppObjFacade(userCtx, ObjectType.ISSUE);
+
+		IAppObjQuery appQuery = facade.createQuery();
+
+		IQueryRestriction restrAnd = null;
+		for (IFilterCriteria filter : filters) {
+			//Restricao por ID de Apontamento/Plano Ação
+			IQueryExpression restr1 = null;
+			if (filter.getAttributeAliasName().equals("IssueID")) {
+				restr1 = QueryRestriction.eq(IIssueAttributeType.ATTR_OBJ_ID, filter.getValue());
 			}
 			
-			//sortMap(resultMap);
-			sortByComparator(resultMap, true);
-			printMap(resultMap);
-				
+			//Restricao por Titulo de Apontamento/Plano de Ação
+			IQueryExpression restr2 = null;
+			if (filter.getAttributeAliasName().equals("name")) {
+				restr2 = QueryRestriction.like(IIssueAttributeType.ATTR_NAME, (String) filter.getValue());
+			}
+			
+			//Adicionando as Restrições
+			if(restr1 != null){
+				IQueryExpression restrFim = QueryRestriction.eq(IIssueAttributeType.BASE_ATTR_VERSION_ACTIVE, true);
+				restrAnd = QueryRestriction.and(restr1, restrFim);
+				appQuery.addRestriction(restrAnd);
+			}
+			if(restr2 != null){
+				IQueryExpression restrFim = QueryRestriction.eq(IIssueAttributeType.BASE_ATTR_VERSION_ACTIVE, true);
+				restrAnd = QueryRestriction.and(restr2, restrFim);
+				appQuery.addRestriction(restrAnd);
+			}
+			if(restr1 != null && restr2 != null){
+				IQueryExpression restrFim = QueryRestriction.eq(IIssueAttributeType.BASE_ATTR_VERSION_ACTIVE, true);
+				restrAnd = QueryRestriction.and(restr1, restr2, restrFim);
+				appQuery.addRestriction(restrAnd);
+			}
+			// appQuery.addRestriction(restr1);
+		}
+		// }
+
+		if (null == restrAnd) {
+			IQueryExpression restr1 = QueryRestriction.eq(IIssueAttributeType.BASE_ATTR_VERSION_ACTIVE, true);
+			appQuery.addRestriction(restr1);
 		}
 
+		IAppObjIterator iterator = appQuery.getResultIterator();
+		while (iterator.hasNext()) {
+			IAppObj issueObj = iterator.next();
+			Integer obj_id = (int) issueObj.getObjectId();
+			Integer version_number = Integer.valueOf(
+					issueObj.getAttribute(IIssueAttributeType.BASE_ATTR_VERSION_NUMBER).getRawValue().intValue());
+			resultMap.put(obj_id, version_number);
+		}
+		appQuery.release();
+
+		printMap(resultMap);
+
+		if (resultMap.size() > 0) {
+
+			// Limpando query original
+			for (IQueryObjectDefinition def : query.getObjectDefinitions()) {
+				query.markAsRemovable(def);
+			}
+			// filters.clear();
+
+			// Criando nova query dinamicamente
+			List<IFilterCriteria> filtersAnd = new ArrayList<IFilterCriteria>();
+			IFilterFactory filterFactory = PersistenceAPI.getFilterFactory();
+			for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
+				IFilterCriteria criteria = filterFactory
+						.and(Arrays.asList(new IFilterCriteria[] {
+								new SimpleFilterCriteria("IssueID", "IssueID", DataLayerComparator.EQUAL,
+										entry.getKey()),
+								new SimpleFilterCriteria("version_number", "version_number", DataLayerComparator.EQUAL,
+										entry.getValue()) }));
+				filtersAnd.add(criteria);
+			}
+
+			query.addFilterCriteria(filterFactory.or(filtersAnd));
+
+		}
+		// Fim REO - 29.01.2017 - EV131567
+
 	}
-	
-	private static void sortMap(Map<Integer, Integer> unsortMap){
-		Object[] array = unsortMap.entrySet().toArray();
-		Arrays.sort(array, new Comparator() {
-		    public int compare(Object o1, Object o2) {
-		        return ((Map.Entry<String, Integer>) o2).getValue()
-		                   .compareTo(((Map.Entry<String, Integer>) o1).getValue());
-		    }
-		});
-		
+
+	private static void printMap(Map<Integer, Integer> map) {
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+		}
 	}
-	
-	private static Map<Integer, Integer> sortByComparator(Map<Integer, Integer> unsortMap, final boolean order)
-    {
-
-        List<Entry<Integer, Integer>> list = new LinkedList<Entry<Integer, Integer>>(unsortMap.entrySet());
-
-        // Sorting the list based on values
-        Collections.sort(list, new Comparator<Entry<Integer, Integer>>()
-        {
-            public int compare(Entry<Integer, Integer> o1,
-                    Entry<Integer, Integer> o2)
-            {
-                if (order)
-                {
-                	return o1.getKey().compareTo(o2.getKey());
-                    //return o1.getValue().compareTo(o2.getValue());
-                }
-                else
-                {
-                	return o2.getKey().compareTo(o1.getKey());
-                    //return o2.getValue().compareTo(o1.getValue());
-                }
-            }
-        });
-
-        // Maintaining insertion order with the help of LinkedList
-        Map<Integer, Integer> sortedMap = new LinkedHashMap<Integer, Integer>();
-        for (Entry<Integer, Integer> entry : list)
-        {
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
-
-        return sortedMap;
-    }
-	
-	private static void printMap(Map<Integer, Integer> map)
-    {
-        for (Entry<Integer, Integer> entry : map.entrySet())
-        {
-            System.out.println("Key : " + entry.getKey() + " Value : "+ entry.getValue());
-        }
-    }
 
 }
