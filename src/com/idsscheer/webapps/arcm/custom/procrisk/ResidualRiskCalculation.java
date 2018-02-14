a024174d3b3436521904c6481923da61797b80af
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-02-14 16:15:49 -0200

    EV133332 - Ajuste no calculo de risco residual 2a linha

--- src/com/idsscheer/webapps/arcm/custom/procrisk/ResidualRiskCalculation.java
+++ src/com/idsscheer/webapps/arcm/custom/procrisk/ResidualRiskCalculation.java
@@ -18,11 +18,16 @@ import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObjFacade;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IViewObj;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IEnumAttribute;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjIterator;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.IAppObjQuery;
+import com.idsscheer.webapps.arcm.bl.models.objectmodel.query.QueryRestriction;
 import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IControlAttributeType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IControlAttributeTypeCustom;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IControlexecutionAttributeType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IControlexecutionAttributeTypeCustom;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.ITestcaseAttributeType;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.ITestdefinitionAttributeTypeCustom;
 import com.idsscheer.webapps.arcm.common.util.ARCMCollections;
 import com.idsscheer.webapps.arcm.common.util.ovid.IOVID;
 import com.idsscheer.webapps.arcm.common.util.ovid.OVIDFactory;
@@ -95,10 +100,145 @@ public class ResidualRiskCalculation {
 		
 	}
 	
-	public String calculateResidual2Line(){
-		return null;
+	public String calculateResidual2Line() throws Exception{
+		
+		int countTotal = 0;
+		int countInef = 0;
+		double risk2line = 0;
+		
+		for(IAppObj controlObj : controlList){
+			
+			countTotal += 1;
+			
+			List<IAppObj> tdList = this.getTestDefFromControl(controlObj);
+			for (IAppObj tdObj : tdList) {
+				
+				List<IAppObj> tstCaseList = this.getTestCaseFromTestDef(tdObj.getObjectId());
+				for (IAppObj tstCaseObj : tstCaseList) {
+					IEnumAttribute ownerStatusAttr = tstCaseObj.getAttribute(ITestcaseAttributeType.ATTR_OWNER_STATUS);
+					IEnumerationItem ownerStatusItem = ARCMCollections.extractSingleEntry(ownerStatusAttr.getRawValue(), true);
+					IEnumAttribute reviewerStatusAttr = tstCaseObj.getAttribute(ITestcaseAttributeType.ATTR_REVIEWER_STATUS);
+					IEnumerationItem reviewerStatusItem = ARCMCollections.extractSingleEntry(reviewerStatusAttr.getRawValue(), true);
+					
+					if(reviewerStatusItem.getId().equals("accepted")){
+						if(ownerStatusItem.getId().equals("noneffective")){
+							if(this.testDefClass(tdObj).equals("1linhadefesa")){
+								countInef += 1;
+							}
+						}
+					}
+				}
+				
+			}
+			
+		}
+		
+		if(countTotal > 0)
+			risk2line = ( countInef / countTotal );
+		
+		return this.riskClassification(risk2line);
+		
 	}
 	
+	private Object testDefClass(IAppObj tdObj) {
+		IEnumAttribute origTestAttr = tdObj.getAttribute(ITestdefinitionAttributeTypeCustom.ATTR_ORIGEMTESTE);
+		IEnumerationItem origTest = ARCMCollections.extractSingleEntry(origTestAttr.getRawValue(), true);
+		
+		return origTest.getId();
+	}
+
+	private List<IAppObj> getTestCaseFromTestDef(long testDefObjID) throws Exception {
+		List<IAppObj> testCaseReturn = new ArrayList<IAppObj>();
+		List<IAppObj> testCaseBuffer = new ArrayList<IAppObj>();
+		
+		Map filterMap = new HashMap();
+		filterMap.put("tdef_id", testDefObjID);
+		
+		IViewQuery query = QueryFactory.createQuery(userContext, "custom_TestDef2TestCase", filterMap, null,
+				true, transaction);
+		
+		try{
+		
+			Iterator itQuery = query.getResultIterator();
+			
+			while(itQuery.hasNext()){
+				
+				IViewObj viewObj = (IViewObj)itQuery.next();
+				long tcID = (Long)viewObj.getRawValue("tcase_id");
+				long tcVersionNumber = (Long)viewObj.getRawValue("tcase_version_number");
+				
+				IAppObjFacade tcFacade = FacadeFactory.getInstance().getAppObjFacade(userContext, ObjectType.TESTCASE);
+				IOVID tcOVID = OVIDFactory.getOVID(tcID, tcVersionNumber);
+				IAppObj tcAppObj = tcFacade.load(tcOVID, true);
+				
+				if(tcAppObj != null){
+					if(tcAppObj.getVersionData().isHeadRevision())
+						testCaseBuffer.add(tcAppObj);
+				}
+				
+			}
+			
+			if(testCaseBuffer.size() > 0){
+				testCaseBuffer.sort(new Comparator<IAppObj>(){
+					@Override
+					public int compare(IAppObj ant, IAppObj post){
+						long antTime = ant.getVersionData().getCreateDate().getTime();
+						long postTime = post.getVersionData().getCreateDate().getTime();
+						return antTime < postTime ? -1 : antTime == postTime ? 0 : 1;
+					}
+				});
+				testCaseReturn.add(testCaseBuffer.get(testCaseBuffer.size() - 1));
+			}
+		
+		}catch(Exception e){
+			query.release();
+			throw e;
+		}finally{
+			query.release();
+		}		
+		
+		return (List<IAppObj>) testCaseReturn;
+	}
+
+	private List<IAppObj> getTestDefFromControl(IAppObj controlObj) {
+		List<IAppObj> testDefReturn = new ArrayList<IAppObj>();
+		
+		IAppObjFacade facade = FacadeFactory.getInstance().getAppObjFacade(userContext, ObjectType.CONTROL);
+		IAppObjQuery query = facade.createQuery();
+		query.addRestriction(
+				QueryRestriction.eq(IControlAttributeType.ATTR_CONTROL_ID, controlObj.getAttribute(IControlAttributeType.ATTR_CONTROL_ID).getRawValue()));
+		query.addRestriction(
+				QueryRestriction.eq(IControlAttributeType.BASE_ATTR_VERSION_ACTIVE, true));
+		
+		IAppObjIterator itQuery = query.getResultIterator();
+		
+		try{
+			
+			while(itQuery.hasNext()){
+								
+				IAppObj ctrlObject = itQuery.next();
+				
+				if(!ctrlObject.getVersionData().isHeadRevision())
+					continue;
+				
+				List<IAppObj> tdList = ctrlObject.getAttribute(IControlAttributeType.LIST_TESTDEFINITIONS).getElements(userContext);
+				for(IAppObj tdObj : tdList){
+					if(!tdObj.getVersionData().isDeleted())
+						testDefReturn.add(tdObj);
+				}
+				
+			}
+			
+		}catch(Exception e){
+			query.release();
+			throw e;
+		}finally{
+			query.release();
+		}		
+		
+		return (List<IAppObj>) testDefReturn;
+	}
+
 	public String calculateResidual3Line(){
 		return null;
 	}
