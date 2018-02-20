6158a4331c9a5dc70521337b4983299ee3e33b76
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-02-20 14:36:21 -0300

    EV133332 - Botão de avaliacao de Risco Residual - Risco Processo

--- src/com/idsscheer/webapps/arcm/ui/components/testmanagement/actioncommands/CustomPREvaluation.java
+++ src/com/idsscheer/webapps/arcm/ui/components/testmanagement/actioncommands/CustomPREvaluation.java
@@ -1,5 +1,8 @@
 package com.idsscheer.webapps.arcm.ui.components.testmanagement.actioncommands;
 
+import java.util.List;
+import java.util.Map;
+
 import com.idsscheer.webapps.arcm.bl.authentication.context.IUserContext;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObj;
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.IAppObjFacade;
@@ -7,7 +10,11 @@ import com.idsscheer.webapps.arcm.bl.models.objectmodel.attribute.IStringAttribu
 import com.idsscheer.webapps.arcm.bl.models.objectmodel.impl.FacadeFactory;
 import com.idsscheer.webapps.arcm.common.constants.metadata.ObjectType;
 import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IHierarchyAttributeTypeCustom;
+import com.idsscheer.webapps.arcm.common.constants.metadata.attribute.IRiskAttributeType;
 import com.idsscheer.webapps.arcm.custom.corprisk.CustomCorpRiskHierarchy;
+import com.idsscheer.webapps.arcm.custom.corprisk.CustomProcRiskResidualCalc;
+import com.idsscheer.webapps.arcm.custom.procrisk.DefLineEnum;
+import com.idsscheer.webapps.arcm.custom.procrisk.RiskAndControlCalculation;
 import com.idsscheer.webapps.arcm.services.framework.batchserver.services.lockservice.LockType;
 import com.idsscheer.webapps.arcm.ui.framework.actioncommands.object.BaseCacheActionCommand;
 import com.idsscheer.webapps.arcm.ui.framework.common.JobUIEnvironment;
@@ -18,7 +25,16 @@ public class CustomPREvaluation extends BaseCacheActionCommand{
 	protected void execute() {
 		// TODO Auto-generated method stub
 		
+		IAppObj riskObj = this.formModel.getAppObj();
+		List<IAppObj> controlList = riskObj.getAttribute(IRiskAttributeType.LIST_CONTROLS).getElements(getFullGrantUserContext());
+		RiskAndControlCalculation objCalc = new RiskAndControlCalculation(controlList, new Double(0), new Double(0));
+		
 		this.notificationDialog.setHeaderMessageKey("message.corp_risk_evaluation.DBI");
+		
+		//Resultados - 1ª Linha
+		Map<String, String> map1Line = objCalc.calculateControlRate(DefLineEnum.LINE_1);
+		
+		this.notificationDialog.addInfo("TESTE");
 		
 	}
 
