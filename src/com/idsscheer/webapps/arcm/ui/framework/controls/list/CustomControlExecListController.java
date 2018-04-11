ac93e4eef2cb00faf326dcdbbb2bb40abf23d6b0
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-04-11 14:38:17 -0300

    EV178278 - Ocultar botao eliminar na lista de ControlExecution

--- src/com/idsscheer/webapps/arcm/ui/framework/controls/list/CustomControlExecListController.java
+++ src/com/idsscheer/webapps/arcm/ui/framework/controls/list/CustomControlExecListController.java
@@ -2,6 +2,9 @@ package com.idsscheer.webapps.arcm.ui.framework.controls.list;
 
 import java.util.Map;
 
+import com.idsscheer.webapps.arcm.bl.authentication.context.IUserRights;
+import com.idsscheer.webapps.arcm.common.constants.metadata.Enumerations.USERROLE_TYPE;
+
 public class CustomControlExecListController extends List {
 
 	public CustomControlExecListController(String p_instanceName, Map<String, String> p_parameters) {
@@ -12,10 +15,12 @@ public class CustomControlExecListController extends List {
 	@Override
 	protected void renderControlBody() {
 		StringBuffer jScript = new StringBuffer();
+		IUserRights userRights = this.environment.getUserContext().getUserRights();
 		
-		
-		jScript.append("$('#header_delete').hide()");
-		this.renderJavaScriptAfterLoad(jScript.toString());
+		if(userRights.hasRole(USERROLE_TYPE.CONTROLEXECUTIONOWNER)){
+			jScript.append("$('#header_delete').hide()");
+			this.renderJavaScriptAfterLoad(jScript.toString());
+		}
 		
 		super.renderControlBody();
 	}
