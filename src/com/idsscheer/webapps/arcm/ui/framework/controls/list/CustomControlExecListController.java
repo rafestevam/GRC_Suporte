58300de3d655b660601e06e6c260548ee9a1b328
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-04-12 11:23:16 -0300

    EV178278 - Ajuste botao eliminar na linha da lista de ControlExecution

--- src/com/idsscheer/webapps/arcm/ui/framework/controls/list/CustomControlExecListController.java
+++ src/com/idsscheer/webapps/arcm/ui/framework/controls/list/CustomControlExecListController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import com.idsscheer.webapps.arcm.bl.authentication.context.IUserRights;
 import com.idsscheer.webapps.arcm.common.constants.metadata.Enumerations.USERROLE_TYPE;
+import com.idsscheer.webapps.arcm.ui.framework.controls.context.IControlContext;
 
 public class CustomControlExecListController extends List {
 
@@ -13,16 +14,36 @@ public class CustomControlExecListController extends List {
 	}
 	
 	@Override
+	public void init(IControlContext p_context) {
+		super.init(p_context);
+	}
+	
+	@Override
 	protected void renderControlBody() {
-		StringBuffer jScript = new StringBuffer();
 		IUserRights userRights = this.environment.getUserContext().getUserRights();
+		StringBuffer jScript = new StringBuffer();
 		
 		if(userRights.hasRole(USERROLE_TYPE.CONTROLEXECUTIONOWNER)){
 			jScript.append("$('#header_delete').hide()");
+			//jScript.append("$(\"a[id*='delete']\").hide()");
 			this.renderJavaScriptAfterLoad(jScript.toString());
 		}
 		
 		super.renderControlBody();
+	}
+	
+	@Override
+	protected void renderControlFooter() {
+		StringBuffer jScript = new StringBuffer();
+		IUserRights userRights = this.environment.getUserContext().getUserRights();
+		
+		if(userRights.hasRole(USERROLE_TYPE.CONTROLEXECUTIONOWNER)){
+			//jScript.append("$('#header_delete').hide()");
+			jScript.append("$(\"a[id*='delete']\").hide()");
+			this.renderJavaScriptAfterLoad(jScript.toString());
+		}
+		
+		super.renderControlFooter();
 	}
 
 }
